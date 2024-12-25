ef76b863b3e610b50e155ea1ec5cc19bab4cd6e4
arwa <arwadridi624@gmail.com>
2024-12-25 11:56:34 +0100

    somme des entiers tableau

---
+++ src/Dridi_Arwa.java
@@ -0,0 +1,9 @@
+public class Dridi_Arwa {
+    public int calculerSomme(int[] tableau) {
+        int somme = 0;
+        for (int nombre : tableau) {
+            somme += nombre;
+        }
+        return somme;
+    }
+}
